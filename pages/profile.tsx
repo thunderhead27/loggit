59fde2ba7b69ae6c4054273436366d4ee7b6755a
thunderhead27 <thunderhead27@gmail.com>
2023-06-14 17:14:00 -0700

    removed border from breakfast

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -814,10 +814,10 @@ export default function ProfileScreen({
                     <h1 className="text-xl border-b-2 border-white mb-2">
                       Breakfast
                     </h1>
-                    <div className="border-b-2 border-white mb-4">
+                    <div className="mb-4">
                       {breakfastCat.map((item: any) => (
                         <div
-                          className="flex flex-row items-center"
+                          className="flex flex-row mb-2 items-center"
                           key={item.id}
                         >
                           <div className="w-[300px] xl:w-[800px]">
@@ -856,141 +856,143 @@ export default function ProfileScreen({
                       ))}
                     </div>
                   </div>
-
                   <div className="flex flex-col items-center">
                     <h1 className="text-xl border-b-2 border-white mb-2">
                       Lunch
                     </h1>
-                    <div className="border-b-2 border-white mb-4"></div>
-                    {lunchCat.map((item: any) => (
-                      <div
-                        className="flex flex-row mb-2 items-center"
-                        key={item.id}
-                      >
-                        <div className="w-[300px] xl:w-[800px]">
-                          {item.servingQty === 1 ? "1" : item.servingQty}{" "}
-                          <span className="underline">
-                            {item.name} ({item.brandName})
-                          </span>{" "}
-                          {item.calories} calories, {item.fat} g fat,{" "}
-                          {item.protein} g protein, {item.carbohydrate} g
-                          carbohydrate, {item.sugar} g sugar, {item.cholesterol}{" "}
-                          mg cholesterol
-                        </div>
+                    <div className="mb-4">
+                      {lunchCat.map((item: any) => (
                         <div
-                          className="cursor-pointer"
-                          onClick={() => {
-                            setOpenDelete(true);
-                            setDeleteId(item.id);
-                          }}
+                          className="flex flex-row mb-2 items-center"
+                          key={item.id}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
+                          <div className="w-[300px] xl:w-[800px]">
+                            {item.servingQty === 1 ? "1" : item.servingQty}{" "}
+                            <span className="underline">
+                              {item.name} ({item.brandName})
+                            </span>{" "}
+                            {item.calories} calories, {item.fat} g fat,{" "}
+                            {item.protein} g protein, {item.carbohydrate} g
+                            carbohydrate, {item.sugar} g sugar,{" "}
+                            {item.cholesterol} mg cholesterol
+                          </div>
+                          <div
+                            className="cursor-pointer"
+                            onClick={() => {
+                              setOpenDelete(true);
+                              setDeleteId(item.id);
+                            }}
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              />
+                            </svg>
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
                   </div>
                   <div className="flex flex-col items-center">
                     <h1 className="text-xl border-b-2 border-white mb-2">
                       Dinner
                     </h1>
-                    <div className="border-b-2 border-white mb-4"></div>
-                    {dinnerCat.map((item: any) => (
-                      <div
-                        className="flex flex-row mb-2 items-center"
-                        key={item.id}
-                      >
-                        <div className="w-[300px] xl:w-[800px]">
-                          {item.servingQty === 1 ? "1" : item.servingQty}{" "}
-                          <span className="underline">
-                            {item.name} ({item.brandName})
-                          </span>{" "}
-                          {item.calories} calories, {item.fat} g fat,{" "}
-                          {item.protein} g protein, {item.carbohydrate} g
-                          carbohydrate, {item.sugar} g sugar, {item.cholesterol}{" "}
-                          mg cholesterol
-                        </div>
+                    <div className="mb-4">
+                      {dinnerCat.map((item: any) => (
                         <div
-                          className="cursor-pointer"
-                          onClick={() => {
-                            setOpenDelete(true);
-                            setDeleteId(item.id);
-                          }}
+                          className="flex flex-row mb-2 items-center"
+                          key={item.id}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
+                          <div className="w-[300px] xl:w-[800px]">
+                            {item.servingQty === 1 ? "1" : item.servingQty}{" "}
+                            <span className="underline">
+                              {item.name} ({item.brandName})
+                            </span>{" "}
+                            {item.calories} calories, {item.fat} g fat,{" "}
+                            {item.protein} g protein, {item.carbohydrate} g
+                            carbohydrate, {item.sugar} g sugar,{" "}
+                            {item.cholesterol} mg cholesterol
+                          </div>
+                          <div
+                            className="cursor-pointer"
+                            onClick={() => {
+                              setOpenDelete(true);
+                              setDeleteId(item.id);
+                            }}
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              />
+                            </svg>
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
                   </div>
                   <div className="flex flex-col items-center">
                     <h1 className="text-xl border-b-2 border-white mb-2">
                       Snacks
                     </h1>
-                    <div className="border-b-2 border-white mb-4"></div>
-                    {snackCat.map((item: any) => (
-                      <div
-                        className="flex flex-row mb-2 items-center"
-                        key={item.id}
-                      >
-                        <div className="w-[300px] xl:w-[800px]">
-                          {item.servingQty === 1 ? "1" : item.servingQty}{" "}
-                          <span className="underline">
-                            {item.name} ({item.brandName})
-                          </span>{" "}
-                          {item.calories} calories, {item.fat} g fat,{" "}
-                          {item.protein} g protein, {item.carbohydrate} g
-                          carbohydrate, {item.sugar} g sugar, {item.cholesterol}{" "}
-                          mg cholesterol
-                        </div>
+                    <div className="mb-4">
+                      {snackCat.map((item: any) => (
                         <div
-                          className="cursor-pointer"
-                          onClick={() => {
-                            setOpenDelete(true);
-                            setDeleteId(item.id);
-                          }}
+                          className="flex flex-row mb-2 items-center"
+                          key={item.id}
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
+                          <div className="w-[300px] xl:w-[800px]">
+                            {item.servingQty === 1 ? "1" : item.servingQty}{" "}
+                            <span className="underline">
+                              {item.name} ({item.brandName})
+                            </span>{" "}
+                            {item.calories} calories, {item.fat} g fat,{" "}
+                            {item.protein} g protein, {item.carbohydrate} g
+                            carbohydrate, {item.sugar} g sugar,{" "}
+                            {item.cholesterol} mg cholesterol
+                          </div>
+                          <div
+                            className="cursor-pointer"
+                            onClick={() => {
+                              setOpenDelete(true);
+                              setDeleteId(item.id);
+                            }}
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              />
+                            </svg>
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
